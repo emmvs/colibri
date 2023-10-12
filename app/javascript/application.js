739dbe1c6c26fbb9bb2cc2d5f1a592d85a8ad679
Victor de Quercize <vdequercize@hotmail.com>
2023-10-12 14:23:53 +0200

    added fetch API to get articles with fixed parameters from newsapi.org. Needs styling and custom parameters option

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,24 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+const results = document.querySelector('#results');
+
+fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=afde1f7406fd42ba8abbd902835cf326")
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data)
+    data.articles.forEach((result) => {
+      const article =
+      `<div>
+        <div style="width:80%;">
+          <img src="${result.urlToImage}" class="card-img-top" />
+          <div>
+            <h6>${result.title} </h6>
+            <p>${result.description}</p>
+          </div>
+        </div>
+      </div>`;
+      results.insertAdjacentHTML('beforeend', article);
+    })
+  });
